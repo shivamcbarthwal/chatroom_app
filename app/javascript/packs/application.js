7eb8c25b5336b789c095bc45082a78c7aa828e06
Shivam Barthwal <shivambarthwal@live.com>
2020-05-15 16:51:46 +0200

    fav icon added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+
+require("jquery")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("semantic-ui-sass")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +19,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require semantic-ui
+//= require jquery
+//= require turbolinks
+
+$(document).on('turbolinks:load', function(){
+    $('.ui .dropdown').dropdown();
+})
